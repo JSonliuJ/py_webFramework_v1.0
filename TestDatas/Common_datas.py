0f3e54f124edd1367c510d96e875814b240dd0ca
JSonliuJ <79450668+JSonliuJ@users.noreply.github.com>
2022-07-11 19:59:32 +0800

    Update Common_datas.py

--- TestDatas/Common_datas.py
+++ TestDatas/Common_datas.py
@@ -5,6 +5,6 @@
 # @Email:   810030907@qq.com
 # @File:    Common_datas.py
 # 全局 --系统访问地址 --登录链接
-web_login_url = "http://120.78.128.25:8765/Index/login.html"
+web_login_url = ""
 
-web_invest_url = "http://120.78.128.25:8765/loan/loan_detail/Id/25750.html"
+web_invest_url = ""
