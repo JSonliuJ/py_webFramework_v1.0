b065969f1187d76e671033c87f3d45115460920c
JSonliuJ <79450668+JSonliuJ@users.noreply.github.com>
2022-07-11 20:04:50 +0800

    Update login_datas.py

--- TestDatas/login_datas.py
+++ TestDatas/login_datas.py
@@ -9,13 +9,13 @@ success_data = {"user":"18684720553","password":"python"}
 
 # 异常用例 - 错误的手机号格式(大于11位、小于11位、为空、不在号码段)
 phone_data = [
-    {"user":"1868472055","password":"python","check":"请输入正确的手机号"},  # 小于11位
-    {"user":"186847205531","password":"python","check":"请输入正确的手机号"}, #大于11位
+    {"user":"18593298797","password":"python","check":"请输入正确的手机号"},  # 小于11位
+    {"user":"185932987977","password":"python","check":"请输入正确的手机号"}, #大于11位
     {"user":"","password":"python","check":"请输入手机号"},  # 手机号为空
     {"user":"11684720553","password":"python","check":"请输入正确的手机号"} # 不在手机号段
 ]
 
 wrongPwd_noReg_data = [
-    {"user":"18684720553","password":"pytho","check":"帐号或密码错误!"},  # 密码错误
-    {"user":"18684720550","password":"python","check":"此账号没有经过授权，请联系管理员!"}, # 账号没有授权
+    {"user":"18593298797","password":"pytho","check":"帐号或密码错误!"},  # 密码错误
+    {"user":"18593298797","password":"python","check":"此账号没有经过授权，请联系管理员!"}, # 账号没有授权
 ]
